e5887bacff117d61d44754632009ea4daa2f08d6
Daniel Ma <drailskid@yahoo.com>
2015-05-04 16:38:00 -0700

    move things around

--- lib/bitbucket/pull/request.rb
+++
@@ -1,9 +0,0 @@
-require "bitbucket/pull/request/version"
-
-module Bitbucket
-  module Pull
-    module Request
-      # Your code goes here...
-    end
-  end
-end
--- lib/bitbucket/pull/request/version.rb
+++
@@ -1,7 +0,0 @@
-module Bitbucket
-  module Pull
-    module Request
-      VERSION = "0.1.0"
-    end
-  end
-end
