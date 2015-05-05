7c5d1514c1605d878914592c4d831163ef900d57
Daniel Ma <drailskid@yahoo.com>
2015-05-05 00:06:02 -0700

    require all support files in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'support'
+
+Dir[File.dirname(__FILE__) + "/support/**.rb"].each{|support_file| require support_file }
+
 require 'bitbucket-pull-request'
