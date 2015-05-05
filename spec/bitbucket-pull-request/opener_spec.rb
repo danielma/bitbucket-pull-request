fca6a11b32afeddc991dd5ec073005f3e5a07317
Daniel Ma <drailskid@yahoo.com>
2015-05-04 18:34:36 -0700

    trying to stub remote

--- spec/bitbucket-pull-request/opener_spec.rb
+++ spec/bitbucket-pull-request/opener_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 
 describe BitbucketPullRequest::Opener do
   before do
-    expect(BitbucketPullRequest::Opener).to receive('path').and_return('danielma/cool-repo')
-    expect(BitbucketPullRequest::Opener).to receive('branch').and_return('feature-branch')
+    expect(BitbucketPullRequest::Remotes).to receive(:get_remotes_raw).and_return(%(origin	https://danielma@bitbucket.org/hrock/hrockchurch.com.git (fetch)
+origin	https://danielma@bitbucket.org/hrock/hrockchurch.com.git (push)
+))
+    expect(described_class).to receive('path').and_return('danielma/cool-repo')
+    expect(described_class).to receive('branch').and_return('feature-branch')
   end
 
   describe 'pull_request_path' do
-    expect(BitbucketPullRequest::Opener.pull_request_path).to eq 'blue'
+    expect(described_class.pull_request_path).to eq 'blue'
+  end
+
+  describe 'source_param' do
+    expect(described_class.source_param).to eq 'source='
   end
 end
